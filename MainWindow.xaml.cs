a98e84c07ceab86265d1df0f79c0392068f844f2
theonlywisdom <mailbaah@gmail.com>
2020-11-11 12:42:01 +0000

    Added MouseDown event

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -51,5 +51,28 @@ namespace MatchGame
                 animalEmoji.RemoveAt(index);
             }
         }
+
+        TextBlock lastTextBlockClikcked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClikcked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClikcked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClikcked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
     }
 }
